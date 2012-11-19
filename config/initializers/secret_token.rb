3fc2366c75c33408e4e6f189714c1af720f8bdc7
hustleio <james@hustle.io>
2012-11-19 22:31:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hustleiotest::Application.config.secret_token = '63090f06007960abf3df9f4e4403123a8ffecb6cfb1641a8bfd9bf70307b2fc46c35054645b250aa01023d84e4fb1275cbddd888497cea585caaba6d50ee94ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hustleiotest::Application.config.session_store :cookie_store, key: '_hustleiotest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hustleiotest::Application.config.session_store :active_record_store
